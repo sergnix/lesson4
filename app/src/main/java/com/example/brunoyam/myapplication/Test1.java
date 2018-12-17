8bad18852adb189a6490b4e084bf7fd405df4167
Sergey Sokolov <sergisokol@gmail.com>
2018-12-17 17:49:10 +0300

    del class

--- app/src/main/java/com/example/brunoyam/myapplication/Test1.java
+++
@@ -1,9 +0,0 @@
-package com.example.brunoyam.myapplication;
-
-import android.util.Log;
-
-public class Test1 {
-
-    public void run() {
-    }
-}
--- app/src/main/java/com/example/brunoyam/myapplication/Test2.java
+++
@@ -1,5 +0,0 @@
-package com.example.brunoyam.myapplication;
-
-public abstract class Test2 {
-    abstract public void run();
-}
